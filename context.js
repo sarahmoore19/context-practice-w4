3797192d2cdd45ea41450022a0ce4d5b81ddfebd
sarahmoore <sarahnmoore19@gmail.com>
2022-10-20 13:04:54 -0400

    predicted outcomes

--- context.js
+++ context.js
@@ -17,21 +17,23 @@ const nemo = new Fish('Nemo');
 /********************************* Scenario 1 *********************************/
 // eat(); // ?
 
+// "undefined eats fish food"
 
 /********************************* Scenario 2 *********************************/
-// nemo.eat = eat;
-// nemo.eat(); // ?
+//nemo.eat = eat;
+//nemo.eat(); // ?
 
-
+// nemo eats fish food
 /********************************* Scenario 3 *********************************/
 // nemo.eat = eat;
 // eat(); // ?
-
+// undefined eats fish food
 
 /********************************* Scenario 4 *********************************/
 // nemo.swim(); // ?
-
+// nemo swimming in the water
 
 /********************************* Scenario 5 *********************************/
 // const swim = nemo.swim;
-// swim(); // ?
+// swim();
+// typeError
